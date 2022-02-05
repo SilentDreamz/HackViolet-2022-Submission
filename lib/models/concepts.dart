aa8e35a72b2f180e042b08757cab23fd402425b1
Sarang-R-119 <sarangvr@yahoo.com>
2022-02-05 13:54:55 -0500

    Model for concepts

---
+++ lib/models/concepts.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Concept {
+  final String name;
+  final String definition;
+  final String example;
+
+  Concept(this.name, this.definition, this.example);
+
+  factory Concept.fromJson(Map<String, dynamic> data) {
+    // note the explicit cast to String
+    // this is required if robust lint rules are enabled
+
+    final name = data['name'] as String;
+    final definition = data['definition'] as String;
+    final example = data['example'] as String;
+
+    return Concept(name: name, definition: definition, example: example);
+  }
+}
