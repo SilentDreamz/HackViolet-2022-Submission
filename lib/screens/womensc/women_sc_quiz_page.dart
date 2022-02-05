70cfd985846f9e63c79b921b9a80399eb8b4a6ec
sunny7dusk <nat233947@gmail.com>
2022-02-05 15:23:26 -0500

    Moved women_sc class to models folder

--- lib/screens/womensc/women_sc_quiz_page.dart
+++ lib/screens/womensc/women_sc_quiz_page.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
-class WomenSCQuiz extends StatelessWidget {
-  const WomenSCQuiz({Key? key, this.data}) : super(key: key);
+class WomenSCQuizPage extends StatelessWidget {
+  const WomenSCQuizPage({Key? key, this.data, this.quesIndex = 0})
+      : super(key: key);
 
   final Data data;
+  final int quesIndex;
+
+  // TODO Functions to check answer correct or not, and then push to
+  void checkAnswerAndPush(ans, ctx) {
+    Navigator.of(ctx).pushNamed('/womenscquiz',
+        arguments: {data, quesIndex, ans == data.quizCorrectAns[quesIndex]});
+  }
+
+  // make sure backing goes back to menu page
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +28,25 @@ class WomenSCQuiz extends StatelessWidget {
               width: 200,
               height: 40,
               decoration: BoxDecoration(
-                  // color: const Color.fromRGBO(, g, b, opacity)
-                  ),
+                  color: const Color.fromRGBO(13, 71, 161, 1),
+                  borderRadius: BorderRadius.circular(16.0)),
+              child: Text(data.question),
             ),
+            ElevatedButton(
+                onPressed: () {
+                  checkAnswerAndPush(data.ans1, context);
+                },
+                child: Text(data.ans1)),
+            ElevatedButton(
+                onPressed: () {
+                  checkAnswerAndPush(data.ans2, context);
+                },
+                child: Text(data.ans2)),
+            ElevatedButton(
+                onPressed: () {
+                  checkAnswerAndPush(data.ans3, context);
+                },
+                child: Text(data.ans3)),
           ],
         ),
       ),
