d60ec5dcc7f28fe3f76cfc73eb0b131743dbd9ab
Sarang-R-119 <sarangvr@yahoo.com>
2022-02-05 22:56:46 -0500

    Modified the concept quiz page

--- lib/models/concepts.dart
+++ lib/models/concepts.dart
@@ -4,8 +4,13 @@ class Concept {
   final String name;
   final String definition;
   final String example;
+  List<Concept> subConcepts;
 
-  Concept(this.name, this.definition, this.example);
+  Concept(this.name, this.definition, this.example, this.subConcepts);
+
+  // Redirecting the constructor
+  Concept.withoutChildren(String name, String definition, String example)
+      : this(name, definition, example, []);
 
   factory Concept.fromJson(Map<String, dynamic> data) {
     // note the explicit cast to String
@@ -14,7 +19,8 @@ class Concept {
     final name = data['name'] as String;
     final definition = data['definition'] as String;
     final example = data['example'] as String;
+    List<Concept> subConcepts = data['subConcepts'] as List<Concept>;
 
-    return Concept(name, definition, example);
+    return Concept(name, definition, example, subConcepts);
   }
 }
--- lib/screens/concept_quiz.dart
+++ lib/screens/concept_quiz.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hackviolet_submission/models/concepts.dart';
 
 class CSConcepts extends StatefulWidget {
   @override
@@ -6,33 +8,63 @@ class CSConcepts extends StatefulWidget {
 }
 
 class _CSConceptsState extends State<CSConcepts> {
-  List<String> concepts = [
-    'Data Types',
-    'Conditional Statements',
-    'Loops',
-    'Functions'
-  ];
+//  List<String> concepts = [
+//    'Data Types',
+//    'Conditional Statements',
+//    'Loops',
+//    'Functions'
+//  ];
+// Dummy data
+  Map<String, dynamic> concepts = {
+    'Data Types': [
+      new Concept.withoutChildren('Integer', 'Int is 3', 'Eg: int x = 3;'),
+      new Concept.withoutChildren(
+          'String', 'String is "s"', 'Eg: string x = "a";'),
+      new Concept.withoutChildren(
+          'Char', 'char is \'s\'', 'Eg: char x = \'s\';')
+    ],
+    'Conditional Statements ': [
+      new Concept.withoutChildren(
+          'if-else statement', 'if is if', 'Eg: if(mind == smart) print name;'),
+    ],
+    'Loops ': [
+      new Concept.withoutChildren('For loop', 'for is for',
+          'Eg: for(var i = 0; i < 10; i++) print name;'),
+      new Concept.withoutChildren(
+          'While loop', 'for is for', 'Eg: while(i < 10) print name;'),
+      new Concept.withoutChildren(
+          'Do While loop', 'for is for', 'Eg: do{ print name;}while(i < 10);'),
+    ]
+  };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Text('Learn CS Concepts'),
-          SizedBox(width: 20),
+          SizedBox(height: 20),
           Expanded(
             child: ListView.builder(
-              itemCount: concepts.length,
+//              shrinkWrap: true,
+              itemCount: concepts.keys.length,
               itemBuilder: (context, index) {
+                String key = concepts.keys.elementAt(index);
+
                 return ListTile(
-                  title: ElevatedButton(
-                    child: Text(concepts[index]),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ConceptQuiz()));
-                    },
+                  title: Center(
+                    child: ElevatedButton(
+                      child: Text(key),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ConceptQuiz(
+                                      concepts: concepts[key],
+                                    )));
+                      },
+                    ),
                   ),
                 );
               },
@@ -45,15 +77,83 @@ class _CSConceptsState extends State<CSConcepts> {
 }
 
 class ConceptQuiz extends StatefulWidget {
+  ConceptQuiz({this.concepts = const []});
+
+  final List<Concept> concepts;
+
   @override
   _ConceptQuizState createState() => _ConceptQuizState();
 }
 
 class _ConceptQuizState extends State<ConceptQuiz> {
+  int _index = 0;
+
+  void _next() {
+    setState(() {
+      _index = _index + 1 >= widget.concepts.length ? _index : _index + 1;
+    });
+  }
+
+  void _previous() {
+    setState(() {
+      _index = _index - 1 < 0 ? _index : _index - 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [Text('Concept'), Text('Definition'), Text('Example')],
+    return Scaffold(
+      body: Center(
+        child: IntrinsicWidth(
+          stepWidth: 24,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            textBaseline: TextBaseline.alphabetic,
+//      children: [Text('Concept'), Text('Definition'), Text('Example')],
+            children: [
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  widget.concepts[_index].name,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                  padding: EdgeInsets.all(20),
+                  child: Text(widget.concepts[_index].definition,
+                      style: TextStyle(color: Colors.white)),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(13),
+                    color: const Color(0xFF343434),
+                  )),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                  padding: EdgeInsets.all(20),
+                  child: Text(widget.concepts[_index].example,
+                      style: TextStyle(color: const Color(0xFFFF8c00))),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(13),
+                    color: const Color(0xFF232323),
+                  )),
+              SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(onPressed: _next, child: Text('Next')),
+              SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(onPressed: _previous, child: Text('Previous'))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
